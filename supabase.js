31e3953370404e274e92912f7f6931b77d9bcfe5
MohamedHossam6273 <mohamedhossamramsuem@gmail.com>
2025-09-18 18:51:41 +0300

    Delete supabase.js

--- supabase.js
+++
@@ -1,6 +0,0 @@
-// Initialize Supabase client
-// Replace with your Supabase project details
-const SUPABASE_URL = "https://achartarfibykqfwcucu.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFjaGFydGFyZmlieWtxZndjdWN1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgxMjMxNTAsImV4cCI6MjA3MzY5OTE1MH0.IeNHDsbQ0rVoF0CcsTf1PpxUdwe5Y32A3YW7N3z3A6M";
-
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
